c4916eb76896e12f73536ae92ee70e4a3063a20b
omkar_nath <omkarnath1123@gmail.com>
2019-03-03 01:57:12 +0530

    vs code launch.json added

--- routes/app.js
+++ routes/app.js
@@ -12,14 +12,18 @@ let app = express();
 // app.use(kue.app);
 
 app.use(logResponseBody);
-app.use(require("body-parser").json({ limit: "5mb" }));
-app.use(require("body-parser").urlencoded({ extended: false }));
+app.use(require("body-parser").json({
+  limit: "10mb"
+}));
+app.use(require("body-parser").urlencoded({
+  extended: false
+}));
 app.use("/", require("./api-routes"));
-process.on("uncaughtException", function(err) {
+process.on("uncaughtException", function (err) {
   console.log(err);
 });
 const port = process.env.PORT || 8080;
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Running SPIDER Rest Hub on port " + port);
 });
 
@@ -28,30 +32,59 @@ app.listen(port, function() {
 //   res.sendFile((process.env.APP_ROOT_PATH || path.join(__dirname, '/build')) + '/index.html');
 // });
 
-(function() {
+(function () {
   let _log = console.log;
   let _error = console.error;
   let _warning = console.warning;
 
-  console.error = function(errMessage) {
-    fs.appendFileSync(
-      `${process.env.LOG_PATH}Spider-REST-API/logs/error.txt`,
-      new Date() + " : " + errMessage + "\n"
-    );
+  console.error = function (errMessage) {
+    if (process.env.NODE_ENV === "production") {
+      if (!fs.existsSync(`${process.env.LOG_PATH}/error.txt`)) {
+        fs.writeFileSync(`${process.env.LOG_PATH}/error.txt`, fileContent, (err) => {
+          if (err) throw err;
+        });
+      }
+      fs.appendFileSync(
+        `${process.env.LOG_PATH}/error.txt`,
+        new Date() + " : " + errMessage + "\n"
+      );
+    }
     _error.apply(console, arguments);
   };
-  console.log = function(logMessage) {
-    fs.appendFileSync(
-      `${process.env.LOG_PATH}Spider-REST-API/logs/log.txt`,
-      new Date() + " : " + logMessage + "\n"
-    );
+  console.log = function (logMessage) {
+    if (process.env.NODE_ENV !== "production") {
+      // delete file if it exeeds 25mb
+      if (!fs.existsSync(`${process.env.LOG_PATH}/log.txt`)) {
+        fs.writeFileSync(`${process.env.LOG_PATH}/log.txt`);
+      }
+      const stats = fs.statSync(`${process.env.LOG_PATH}/log.txt`);
+      const fileSizeInBytes = stats.size;
+      const fileSizeInMegabytes = fileSizeInBytes / 1000000.0;
+      if (fileSizeInMegabytes > 25) {
+        fs.unlink(`${process.env.LOG_PATH}/log.txt`, (err) => {
+          if (err) throw err;
+          console.log('path/file.txt was deleted');
+        });
+      }
+      fs.appendFileSync(
+        `${process.env.LOG_PATH}/log.txt`,
+        new Date() + " : " + logMessage + "\n"
+      );
+    }
     _log.apply(console, arguments);
   };
-  console.warning = function(warnMessage) {
-    fs.appendFileSync(
-      `${process.env.LOG_PATH}Spider-REST-API/logs/warning.txt`,
-      new Date() + " : " + warnMessage + "\n"
-    );
+  console.warning = function (warnMessage) {
+    if (process.env.NODE_ENV === "production") {
+      if (!fs.existsSync(`${process.env.LOG_PATH}/warning.txt`)) {
+        fs.writeFileSync(`${process.env.LOG_PATH}/warning.txt`, fileContent, (err) => {
+          if (err) throw err;
+        });
+      }
+      fs.appendFileSync(
+        `${process.env.LOG_PATH}/warning.txt`,
+        new Date() + " : " + warnMessage + "\n"
+      );
+    }
     _warning.apply(console, arguments);
   };
 })();
@@ -60,11 +93,11 @@ function logResponseBody(req, res, next) {
   const oldWrite = res.write,
     oldEnd = res.end;
   const chunks = [];
-  res.write = function(chunk) {
+  res.write = function (chunk) {
     chunks.push(chunk);
     oldWrite.apply(res, arguments);
   };
-  res.end = function(chunk) {
+  res.end = function (chunk) {
     if (chunk) chunks.push(chunk);
     const body = Buffer.concat(chunks).toString("utf8");
     console.log(
@@ -77,7 +110,7 @@ function logResponseBody(req, res, next) {
   next();
 }
 
-app.listen(function() {
+app.listen(function () {
   console.log("Environment Loaded | " + process.env.TEST || "development");
   console.log("App current environment  | " + process.env.NODE_ENV || "local");
   console.log(
